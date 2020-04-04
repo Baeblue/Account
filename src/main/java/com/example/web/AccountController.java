fe2324e137f5522b1109264f7ba3efe405521630
Baeblue <44233657+Baeblue@users.noreply.github.com>
2020-04-04 17:55:27 +0900

    Add comment to AccountController.java

--- src/main/java/com/example/web/AccountController.java
+++ src/main/java/com/example/web/AccountController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// This is AccountController
 @Controller
 @RequestMapping("accounts")
 public class AccountController {
